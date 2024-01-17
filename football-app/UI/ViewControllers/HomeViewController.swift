8cf6877ba43df7b40876006b1c8595e9340eed3a
James <95doanphuong@gmail.com>
2024-01-17 22:46:47 +0700

    Update filter match and team by name (#1)
    * Update filter match and team by name
    
    * fixing .gitignore
    
    ---------
    
    Co-authored-by: James.Doan <James.Doan@gmail.com>

--- football-app/UI/ViewControllers/HomeViewController.swift
+++ football-app/UI/ViewControllers/HomeViewController.swift
@@ -20,6 +20,7 @@ class HomeViewController: UIViewController {
     private lazy var allTeamView: AllTeamView = makeTeamListView()
     private lazy var allMatchView: MatchesView = makeAllMatchView()
     private lazy var tabsView: FlexibleHeightTabView = makeTabsView()
+    private lazy var searchView: UISearchBar = makeSearchView()
 
     private let cancelBag: CancelBag = CancelBag()
 
@@ -30,9 +31,9 @@ class HomeViewController: UIViewController {
     // MARK: - View Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let output = viewModel.transform(input: searchView.searchTextField.texPublisher)
 
-        // Listening data
-        viewModel.getAllTeam()
+        output.teames
             .sinkToResult { [weak self] result in
                 switch result {
                 case .success(let list):
@@ -47,8 +48,7 @@ class HomeViewController: UIViewController {
             }
             .store(in: cancelBag)
 
-        viewModel
-            .getAllMatch()
+        output.matches
             .sinkToResult { [weak self] resutl in
                 switch resutl {
                 case .success(let matches):
@@ -58,6 +58,7 @@ class HomeViewController: UIViewController {
                 }
             }
             .store(in: cancelBag)
+        NotificationCenter.default.post(name: UITextField.textDidChangeNotification, object: searchView.searchTextField)
     }
 
     override func viewDidLoad() {
@@ -65,14 +66,22 @@ class HomeViewController: UIViewController {
         title = "Home"
         view.backgroundColor = UIColor.white
 
-        view.addSubview(tabsView)
-        tabsView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                        leading: view.leadingAnchor,
-                        bottom: view.safeAreaLayoutGuide.bottomAnchor,
-                        trailing: view.trailingAnchor)
+        let containerView: UIStackView = UIStackView(arrangedSubviews: [searchView, tabsView])
+        containerView.axis = .vertical
+        view.addSubview(containerView)
+        containerView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                             leading: view.leadingAnchor,
+                             bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                             trailing: view.trailingAnchor)
     }
 
     // MARK: - Make views
+    private func makeSearchView() -> UISearchBar {
+        let searchView: UISearchBar = UISearchBar()
+        searchView.constrainHeight(80)
+        return searchView
+    }
+
     private func makeTabsView() -> FlexibleHeightTabView {
         let view = FlexibleHeightTabView(titles: ["Teams", "AllMatch"], style: .segmented)
         // Add content view
@@ -121,5 +130,18 @@ extension UIViewController {
         present(playerViewController, animated: true) {
             player?.play()
         }
+    }
+}
+
+extension UISearchTextField {
+    var texPublisher: AnyPublisher<String, Error> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .map(\.object)
+            .map { $0 as! UITextField }
+            .map(\.text)
+            .compactMap { $0 }
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
     }
 }
--- football-app/UI/ViewModels/HomeViewModel.swift
+++ football-app/UI/ViewModels/HomeViewModel.swift
@@ -11,13 +11,48 @@ import Combine
 protocol HomeViewModel: MatchesViewModel {
     func getAllTeam() -> AnyPublisher<[FootballTeam], Error>
     func getAllMatch() -> AnyPublisher<[FootballMatch], Error>
+
+    func transform(input searchText: AnyPublisher<String, Error>) -> HomeViewModelOutput
 }
 
 protocol MatchesViewModel {
     func getLogo(of team: String?) -> AnyPublisher<String?, Error>
 }
 
+struct HomeViewModelOutput {
+    let matches: AnyPublisher<[FootballMatch], Error>
+    let teames: AnyPublisher<[FootballTeam], Error>
+}
+
 class RealHomeViewModel: HomeViewModel {
+    func transform(input searchText: AnyPublisher<String, Error>) -> HomeViewModelOutput {
+        let matches = searchText
+            .debounce(for: .seconds(0.01), scheduler: RunLoop.main)
+            .combineLatest(footballService.getAllMatch())
+            .map { searchText, matches -> [FootballMatch] in
+                guard !searchText.isEmpty else {
+                    return matches
+                }
+                return matches.filter { ($0.away?.contains(searchText) ?? false) || (($0.home?.contains(searchText) ?? false))}
+            }
+            .extractUnderlyingError()
+            .eraseToAnyPublisher()
+    
+        let teames = searchText
+            .debounce(for: .seconds(0.01), scheduler: RunLoop.main)
+            .combineLatest(footballService.getAllTeam())
+            .map { searchText, teames -> [FootballTeam] in
+                guard !searchText.isEmpty else {
+                    return teames
+                }
+                return teames.filter { $0.name?.contains(searchText) ?? false}
+            }
+            .extractUnderlyingError()
+            .eraseToAnyPublisher()
+
+        return HomeViewModelOutput(matches: matches, teames: teames)
+    }
+
     let footballService: FootballService
     
     init(footballService: FootballService = RealFootballService()) {
